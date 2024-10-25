b7da2c0f6a652a8259a4a743a17e353c09433946
vt_dylanlau <dylanlau@vt.edu>
2024-10-24 23:06:24 -0400

    some organization
    reworked the csvs too

--- csv_to_map.cpp
+++ csv_to_map.cpp
@@ -2,29 +2,44 @@
 #include <iostream>
 #include <string.h>
 
-int main(){
-	rapidcsv::Document dealer_table("Dealer Table.csv"); //make it skip empty lines (unnecessary)
-	
-	rapidcsv::Document nondealer_table("Non Dealer Table.csv");
 
- 	// std::vector<float> dealer_col = dealer_table.GetColumn<float>(11);
+void read_table_csv(std::string table_file)
+{
+	printf("Table being read: %s\n", table_file.c_str());
+	rapidcsv::Document table_doc(table_file);
 
+	for (size_t row = 0; row < table_doc.GetRowCount(); row++) { //iterate through each row
+		for (size_t col = 1; col < table_doc.GetColumnCount(); col++) { //iterate through each column
+		
+			std::string table_score_cell = table_doc.GetCell<std::string>(col, row); //get the cell at that row/ col
 
-	for (size_t row = 0; row < dealer_table.GetRowCount(); row++)
-	{
-		for (size_t col = 0; col < dealer_table.GetColumnCount(); col++)
-		{
-			std::string dealer_score_cell = dealer_table.GetCell<std::string>(col, row);
+			std::string fu_amount = table_doc.GetColumnName(col);
 
-			if (dealer_score_cell == NULL){
-				//continue
-				
-			}
-			// string t = "test";
-			// printf(dealer_score_cell);
-			printf( "%d han, %d fu: %s \n", row, col, dealer_score_cell.c_str());
-			// std::cout << dealer_score_cell;
+			printf( "%d han, %s fu: %s \n", row + 1, fu_amount.c_str(), table_score_cell.c_str()); //the data of the cell
 		}
 	}
-	
+}
+// void read_high_hands_table(rapidcsv::Document high_hand_table_doc){
+
+// }
+
+int main(){
+
+	std::string dealer_table_file("Dealer Table.csv");
+	std::string dealer_high_hands("Dealer Table.csv");
+
+	// rapidcsv::Document dealer_table("Dealer Table.csv");
+	// rapidcsv::Document dealer_high_hands("Dealer High Hands.csv");
+
+	std::string nondealer_table_file("Non Dealer Table.csv");
+	std::string nondealer_high_hands("Non Dealer High Hands.csv");
+
+
+	// rapidcsv::Document nondealer_table("Non Dealer Table.csv");
+	// rapidcsv::Document nondealer_high_hands("Non Dealer High Hands.csv");
+
+	read_table_csv(dealer_table_file);
+
+	read_table_csv(nondealer_table_file);
+
 }
