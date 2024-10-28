22db0cc3bdc96fa32794ad9933b0973b254acd14
vt_dylanlau <dylanlau@vt.edu>
2024-10-27 20:08:51 -0400

    some changes
    idk what i wrote i cant remember right now

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 csv_to_map:
 	g++ csv_to_map.cpp Score.cpp -o csv_to_map.exe 
+
--- csv_to_map.cpp
+++ csv_to_map.cpp
@@ -3,8 +3,41 @@
 #include <string.h>
 // #include "Score.cpp"
 #include "Score.h"
+#include <array>
 
+std::array<int, 3> read_cell(std::string input_cell_string){
+	
 
+	std::string tsumo_str 	= input_cell_string.substr(0, input_cell_string.find(" "));
+	std::string ron_str 	= input_cell_string.substr(input_cell_string.find(" ") + 1, input_cell_string.size());
+	
+	int ron_value = std::stoi(ron_str.c_str());
+	int tsumo_nondealer_amount;
+	int tsumo_dealer_amount;
+	
+	if (tsumo_str.find("/") == std::string::npos)
+	{
+		//go here when split "/" doesn't exist
+		// printf("/ split doesn't exist\n");
+		int tsumo_all = std::stoi(tsumo_str);
+
+		tsumo_dealer_amount = tsumo_all;
+		tsumo_nondealer_amount = tsumo_all;
+	}
+	else{
+		int delimiter_index = tsumo_str.find("/");
+		//split string and assign them to score
+		tsumo_dealer_amount = std::stoi(tsumo_str.substr(delimiter_index + 1, tsumo_str.size()));
+		// printf("\ttsumo_dealer_amount: |%d|\n", tsumo_dealer_amount);
+		
+		tsumo_nondealer_amount = std::stoi(tsumo_str.substr(0, delimiter_index));
+	
+	}
+
+	std::array<int, 3> points_array{tsumo_nondealer_amount,tsumo_dealer_amount,ron_value};	
+
+	return points_array;
+}
 int create_han_fu_hash(int han, int fu)
 {
 	//64 bit number
@@ -32,64 +65,14 @@ void read_table_csv(std::map<int, Score> score_map, std::string table_file) {
 			// printf("fu: |%d|\n", fu);
 			int hf_hash = create_han_fu_hash(han, fu);
 			
+			std::array<int, 3> cell_array = read_cell(table_score_cell);
 
-			std::string tsumo_str 	= table_score_cell.substr(0, table_score_cell.find(" "));
-			std::string ron_str 	= table_score_cell.substr(table_score_cell.find(" ") + 1, table_score_cell.size());
+			// printf( "%d han, %d fu: \n\tTsumo Points from Dealer: %d\n\tTsumo Points from Others: %d\n\tRon Points: %d\n", han, fu, tsumo_dealer_amount, tsumo_nondealer_amount, ron_value); //the data of the cell
 			
-			printf("tsumo str: |%s|\nron str: |%s|\n", tsumo_str.c_str(), ron_str.c_str());
-			
-			int ron_value;
-			if (ron_str == ("NULL"))
-			{
-				ron_value = 0;
-			}
-			else{
-				ron_value = std::stoi(ron_str.c_str());
-			}
-			int tsumo_nondealer_amount;
-			int tsumo_dealer_amount;
-			
-			if (tsumo_str.find("NULL") != std::string::npos)
-			{
-				//this means that null is found
-				tsumo_dealer_amount = 0;
-				tsumo_nondealer_amount = 0;
-
-			}
-			else{
-
-
-				if (tsumo_str.find("/") != std::string::npos)
-				{
-					int tsumo_all = std::stoi(tsumo_str);
-
-					tsumo_dealer_amount = tsumo_all;
-					tsumo_nondealer_amount = tsumo_all;
-				}
-				else{
-					int delimiter_index = tsumo_str.find("/");
-					//split string and assign them to score
-					tsumo_dealer_amount = std::stoi(tsumo_str.substr(delimiter_index + 1, tsumo_str.size()));
-					tsumo_nondealer_amount = std::stoi(tsumo_str.substr(0, delimiter_index));
-				
-				}
-
-			}
-			printf( "%d han, %d fu: \n\tTsumo Points from Dealer: %d\n\tTsumo Points from Others: %d\n\tRon Points: %d\n", han, fu, tsumo_dealer_amount, tsumo_nondealer_amount, ron_value); //the data of the cell
-
-
 			// Score cell_score = new Score(tsumo_nondealer_amount, tsumo_dealer_amount, ron_value );
-			Score cell_score(tsumo_nondealer_amount, tsumo_dealer_amount, ron_value);
+			Score cell_score(cell_array[0], cell_array[1], cell_array[2]);
 			std::pair<int, Score> cell_pair = std::make_pair(hf_hash, cell_score);
 			score_map.insert(cell_pair);
-
-			// score_map.insert(std::make_pair(han_fu_hash, cell_score));
-			// printf("tsumo: %d\n", tsumo_val);
-			// printf("ron: %d\n", ron_val);
-
-
-			// Score score();
-
 
 		}
 	}
@@ -100,9 +83,15 @@ void read_HH_table(std::map<int, Score> score_map, std::string high_hand_table_d
 	rapidcsv::Document hh_doc(high_hand_table_doc);
 
 	for (size_t row = 0; row < hh_doc.GetRowCount(); row++) { //iterate through each row
-		std::string score_cell = hh_doc.GetCell<std::string>(1, row); //get the cell at that row/ col
+		std::string cell_str = hh_doc.GetCell<std::string>(1, row); //get the cell at that row/ col
 
-		printf("%d Han: %s\n", row, score_cell.c_str());
+
+		std::array<int, 3> hh_cell_array = read_cell(cell_str);
+
+		Score cell_score(hh_cell_array[0], hh_cell_array[1], hh_cell_array[2]);
+		
+		int hh_hf_hash = create_han_fu_hash(row + 5, 0);
+		// printf("%d Han: %s\n", row + 5, cell_str.c_str());
 
 	}
 }
